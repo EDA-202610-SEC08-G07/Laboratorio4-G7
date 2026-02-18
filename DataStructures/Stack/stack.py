869f1d7dc548b3945a96b0628f20006df23bcea9
jsaldanam <j.saldanam@uniandes.edu.co>
2026-02-17 20:45:11 -0500

    cambios stack para array_list

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,19 +1,29 @@
-from DataStructures.List import single_linked_list as lt
+from DataStructures.List import array_list as rt
+#usar rt para array_list, cambiar push1 a push y top1 a top 
+#usar lt para linked_list, cambair push1 a push y top1 a top
 def new_stack():
-    stacky = lt.new_list()
+    stacky = rt.new_list()
     return stacky
-def push(my_stack,element):
+
+def push1(my_stack,element):
     if my_stack["size"] == 0:
         my_stack = lt.add_first(my_stack,element)
     else:
         my_stack["last"] = my_stack["first"]
         my_stack = lt.add_first(my_stack,element)
     return my_stack
+def push(my_stack,element):
+    if my_stack["size"] == 0:
+        my_stack = rt.add_first(my_stack,element)
+    else:
+        
+        my_stack = rt.add_first(my_stack,element)
+    return my_stack
 def pop(my_stack):
     if my_stack["size"] == 0:
         return 'EmptyStructureError: stack is empty'
     else:
-        my_stack = lt.remove_first(my_stack)
+        my_stack = rt.remove_first(my_stack)
         return my_stack
 def is_empty(my_stack):
     if my_stack["size"] == 0:
@@ -22,11 +32,17 @@ def is_empty(my_stack):
         return False
     
     
-def top(my_stack):
+def top1(my_stack):
     if my_stack["size"] == 0:
         return 'EmptyStructureError: stack is empty'
     else:
         return my_stack["first"]["info"]
 def size(my_stack):
     return my_stack["size"]
-    
+def top(my_stack):
+    if my_stack["size"] == 0:
+        return 'EmptyStructureError: stack is empty'
+    else:
+        return rt.first_element(my_stack)
+def size(my_stack):
+    return my_stack["size"]
